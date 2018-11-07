56defe24fc3f8a011e23f5d27eed63bc188348e3
ksdshpx <southeast_px@163.com>
2018-11-07 14:14:56 +0800

    Controller之getPara方法！

--- src/main/java/cn/ksdshpx/pjfinal/controller/FrontController.java
+++ src/main/java/cn/ksdshpx/pjfinal/controller/FrontController.java
@@ -6,9 +6,10 @@ import com.jfinal.core.Controller;
  * @author peng.x
  * @date 2018年11月6日 上午10:49:40
  */
-public class FrontController extends Controller{
+public class FrontController extends Controller {
 	public void frontMethod() {
-		setAttr("front", "我是前端页面");
+		String msg = getPara("msg", "defaultMsg");
+		setAttr("front", "我是前端页面----->" + msg);
 		render("front.html");
 	}
 }
